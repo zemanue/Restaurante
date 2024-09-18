3b6201430dd5778b9c9ebcbf5169b991d8b3719d
zemanue <manureco.97@gmail.com>
2024-09-18 14:23:24 +0200

    Create Customer.java
    Create Customer class that represents each customer coming to the restaurant, with satisfaction level and patience level (and a preference by seat as an idea in comments)

---
+++ restaurante/Customer.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Customer {
+    Random random = new Random();
+    private int satisfactionLevel;
+    private int patienceLevel;
+    //private boolean prefersWindow;
+    
+    public Customer() {
+        this.patienceLevel = random.nextInt(1, 11);
+        //this.prefersWindow = random.nextBoolean();
+    }
+    public Customer(int patienceLevel/*, boolean prefersWindow*/) { 
+        this.patienceLevel = patienceLevel;
+        //this.prefersWindow = prefersWindow; 
+    }
+
+    public int getSatisfactionLevel() {
+        return satisfactionLevel;
+    }
+    public void setSatisfactionLevel(int satisfactionLevel) {
+        this.satisfactionLevel = satisfactionLevel;
+    }
+
+    public int getPatienceLevel() { 
+        return patienceLevel; 
+    }
+    public void setPatienceLevel(int patienceLevel) {
+        this.patienceLevel = patienceLevel;
+    }
+
+    /*public boolean getprefersWindow() {
+        return prefersWindow;
+    }*/
+    /*public void setprefersWindow(boolean prefersWindow) {
+        this.prefersWindow = prefersWindow;
+    } */
+
+}

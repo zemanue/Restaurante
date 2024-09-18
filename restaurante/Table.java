3f17d69895b1b4b816787de0b985a7bcd06c8504
zemanue <manureco.97@gmail.com>
2024-09-18 15:19:00 +0200

    Update Table.java
    - All variables changed to private
    - New static variable, totalSatisfaction, that stores the sum of the satisfaction of every client each time a table is freed.
    - peopleSeated variable is now called occupiedSeats. peopleSeated is now an array of Customer objects.
    - occupyTable() method now creates new Customers and fill the peopleSeated array
    - freeTable() method now clear the peopleSeated array

--- restaurante/Table.java
+++ restaurante/Table.java
@@ -1,9 +1,14 @@
+// import java.util.ArrayList;
+import java.util.Random;
 
 public class Table {
+    Random random = new Random();
     private int tableNumber;
-    protected boolean occupied;
+    private boolean occupied;
     private int maxCapacity;
-    protected int peopleSeated;
+    private Customer[] peopleSeated;
+    private int occupiedSeats;
+    private static int totalSatisfaction = 0;
     
     public int getTableNumber() {
         return tableNumber;
@@ -11,39 +16,57 @@ public class Table {
     public void setTableNumber(int tableNumber) {
         this.tableNumber = tableNumber;
     }
+
     public boolean isOccupied() {
         return occupied;
     }
     public void setOccupied(boolean occupied) {
         this.occupied = occupied;
-    }    
+    }
+
     public int getMaxCapacity() {
         return maxCapacity;
     }
     public void setMaxCapacity(int maxCapacity) {
         this.maxCapacity = maxCapacity;
     }
-    public int getPeopleSeated() {
+
+    public Customer[] getPeopleSeated() {
         return peopleSeated;
     }
-    public void setPeopleSeated(int peopleSeated) {
+    public void setPeopleSeated(Customer[] peopleSeated) {
         this.peopleSeated = peopleSeated;
     }
-    
 
-    //Constructor con maxCapacity por defecto = 2;
+    public int getOccupiedSeats() {
+        return occupiedSeats;
+    }
+    public void setOccupiedSeats(int occupiedSeats) {
+        this.occupiedSeats = occupiedSeats;
+    }
+
+    public static int getTotalSatisfaction() {
+        return totalSatisfaction;
+    }
+    public static void setTotalSatisfaction(int totalSatisfaction) {
+        Table.totalSatisfaction = totalSatisfaction;
+    }    
+
+    //Constructor with maxCapacity by default = 2;
     public Table(int tableNumber) {
         this.tableNumber = tableNumber;
         this.occupied = false;
         this.maxCapacity = 2;
-        this.peopleSeated = 0;
+        this.peopleSeated = new Customer[this.maxCapacity];
+        this.occupiedSeats = 0;
     }
     
     public Table(int tableNumber, int maxCapacity) {
         this.tableNumber = tableNumber;
         this.maxCapacity = maxCapacity;
         this.occupied = false;
-        this.peopleSeated = 0;
+        this.peopleSeated = new Customer[maxCapacity];
+        this.occupiedSeats = 0;
     }
 
     public boolean occupyTable(int peopleSeated) {
@@ -56,22 +79,36 @@ public class Table {
         }
         else {
             this.occupied = true;
-            this.peopleSeated = peopleSeated;
+            this.occupiedSeats = peopleSeated;
+            for (int i = 0; i < peopleSeated; i++) {
+                this.peopleSeated[i] = new Customer();
+            }
             System.out.println("Mesa " + tableNumber + " ocupada por " + peopleSeated + " personas.");
             return true;
         }
     }
 
     public void freeTable() {
+        for (int i = 0; i < peopleSeated.length; i++) {
+            this.peopleSeated[i].setSatisfactionLevel(random.nextInt(1, 6));
+            totalSatisfaction += this.peopleSeated[i].getSatisfactionLevel();
+            this.peopleSeated[i] = null;
+        }
         this.occupied = false;
-        this.peopleSeated = 0;
+        this.occupiedSeats = 0;
         System.out.println("Mesa " + tableNumber + " liberada.");
     }
+
     public void freeTable(boolean message) {
+        for (int i = 0; i < occupiedSeats; i++) {
+            peopleSeated[i].setSatisfactionLevel(random.nextInt(1, 6));
+            totalSatisfaction += peopleSeated[i].getSatisfactionLevel();
+            peopleSeated[i] = null;
+        }
         this.occupied = false;
-        this.peopleSeated = 0;
+        this.occupiedSeats = 0;
         if (message) {
-        System.out.println("Mesa " + tableNumber + " liberada.");
+            System.out.println("Mesa " + tableNumber + " liberada.");
         }
     }
 }

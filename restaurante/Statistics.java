99f8c87f01bb55f22f2e1ff4f4882b79a8c74377
zemanue <manureco.97@gmail.com>
2024-11-06 21:45:04 +0100

    Create Statistics.java
    New class to separate all the statistics stuff from the Restaurant class. Changes in the rest of the classes are still pending.

---
+++ restaurante/Statistics.java
@@ -0,0 +1,51 @@
+public class Statistics {
+    private int occupiedTables = 0;
+    private int totalCustomers = 0;
+    private int totalSatisfaction = 0;
+    private int timesWithoutTable = 0;
+    private int windowPreference = 0;
+    private int timesWindowPreferenceGiven = 0;    
+
+    public void incrementOccupiedTables() {
+        occupiedTables++;
+    }
+
+    public void addCustomer(int numCustomers) {
+        totalCustomers += numCustomers;
+    }
+
+    public void addSatisfaction(int satisfaction) {
+        totalSatisfaction += satisfaction;
+    }
+
+    public void incrementTimesWithoutTable() {
+        timesWithoutTable++;
+    }
+
+    public void incrementWindowPreference() {
+        windowPreference++;
+    }
+
+    public void incrementTimesWindowPreferenceGiven() {
+        timesWindowPreferenceGiven++;
+    }
+
+    public void showStatistics() {
+        System.out.println("Estadísticas:");
+        System.out.println("- Mesas ocupadas durante el día: " + occupiedTables);
+        System.out.println("- Clientes atendidos: " + totalCustomers);
+        double averageSatisfaction = totalCustomers > 0 ? (double) totalSatisfaction / totalCustomers : 0;
+        System.out.println("- Satisfacción media: " + Math.round(averageSatisfaction * 10.0) / 10.0 + "/5");
+        System.out.println("- Veces que un grupo de clientes se quedó sin mesa: " + timesWithoutTable);
+        System.out.println("- Preferencias de ventana dadas: " + timesWindowPreferenceGiven + "/" + windowPreference);
+    }
+
+    public void reset() {
+        occupiedTables = 0;
+        totalCustomers = 0;
+        totalSatisfaction = 0;
+        timesWithoutTable = 0;
+        windowPreference = 0;
+        timesWindowPreferenceGiven = 0;
+    }
+}
